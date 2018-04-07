48dc531fc680b7518b03cef5573658a152fea2da
Joel Santos <joelgtsantos@gmail.com>
2018-04-06 19:31:21 -0600

    Add src to load database

--- server.js
+++ server.js
@@ -6,27 +6,29 @@ const app = express();
 
 import graphqlHTTP from 'express-graphql';
 import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
+import {
+  NodeInterface,
+  UserType,
+  PostType
+} from './src/types';
+
+import * as loaders from './src/loaders';
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   description: 'The root query',
   fields: {
-    viewer: {
-      type: GraphQLString,
-      resolve() {
-        return 'viewer!';
+    node: {
+      type: NodeInterface,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(source, args) {
+        return loaders.getNodeById(args.id);
       }
-    }
-  },
-  node: {
-    type: GraphQLString,
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLID)
-      }
-    },
-    resolve(source, args) {
-      return inMemoryStore[args.key];
     }
   }
 });
@@ -56,6 +58,7 @@ const RootMutation = new GraphQLObjectType({
 });
 
 const Schema = new GraphQLSchema({
+  types: [UserType, PostType],
   query: RootQuery,
   mutation: RootMutation,
 });
